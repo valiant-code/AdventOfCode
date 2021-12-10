edb7a100b39b74350079b46481b2739aae208196
Dallas Sanchez <dman012@gmail.com>
2021-12-10 15:11:08 -0500

    reimplement day 10 with using a stack instead of recursion for 100x better code

--- 2021/src/main/kotlin/Day10.kt
+++ 2021/src/main/kotlin/Day10.kt
@@ -10,95 +10,56 @@ private val chunkChars = mapOf('(' to ')', '[' to ']', '{' to '}', '<' to '>')
 private val errorPointMap = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
 private val addedPointMap = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
 
-
-private fun incompleteEx(expectedClose: Char) = Exception("incomplete line, expected $expectedClose");
-
-private fun getClosingChar(line: String, pos: Int): Pair<Char, Int> {
-    var position = pos;
-    val opener = line[position];
-    val expectedCloser = chunkChars[opener]
-    val closer: Pair<Char, Int>?;
-    if (line.length - 1 < position + 1) throw incompleteEx(expectedCloser ?: '8');
-    var nextChar = line.getOrNull(position + 1) ?: throw incompleteEx(expectedCloser ?: '8');
-    while (nextChar in chunkChars.keys) {
-        val childsClose = getClosingChar(line, position + 1)
-        position = childsClose.second;
-        nextChar = line.getOrNull(position + 1) ?: throw incompleteEx(expectedCloser ?: '8');
+private fun getInvalidClosers(line: String): Char? {
+    val openers = chunkChars.keys;
+    val openersStack = mutableListOf<Char>()
+    for (char in line) {
+        if (char in openers) {
+            openersStack.add(char)
+        } else {
+            val lastOpener = openersStack.removeLast();
+            val neededCloser = chunkChars[lastOpener]
+            if (neededCloser != char) {
+                return char
+            }
+        }
     }
-    closer = nextChar to position + 1
-
-    if (closer.first != expectedCloser) {
-        throw Exception("Expected ${expectedCloser}, but found ${closer.first} instead")
-    }
-
-    return closer;
+    return null;
 }
 
 private fun partOne(pt: Int = 1) {
     val input = InputUtil.readFileAsStringList("day10/input.txt")
-    val exceptions = mutableListOf<String>()
+    val answer = input.map(::getInvalidClosers)
+        .filterNotNull()
+        .map { c-> errorPointMap.getOrDefault(c, 0) }.sum();
+    println("pt $pt answer: ${answer colorize ConsoleColor.CYAN_BOLD}")
+}
 
-    //A corrupted line is one where a chunk closes with the wrong character
-    for (line in input) {
-        try {
-            var i = 0;
-            while (i < line.length) {
-                i = getClosingChar(line, i).second + 1;
+private fun getNeededCharsToComplete(line: String): List<Char> {
+    val openers = chunkChars.keys;
+    val openersStack = mutableListOf<Char>()
+    for (char in line) {
+        if (char in openers) {
+            openersStack.add(char)
+        } else {
+            val lastOpener = openersStack.removeLast();
+            val neededCloser = chunkChars[lastOpener]
+            if (neededCloser != char) {
+                return emptyList()
             }
-        } catch (e: Exception) {
-            exceptions.add(e.message ?: e.toString())
         }
     }
-
-
-    val answer = exceptions.filter { it.matches(Regex(".*Expected ., but found . instead")) }
-        .map { it[22] }
-        .map { errorPointMap.getOrDefault(it, 0) }.sum();
-    println("pt $pt answer: ${answer colorize ConsoleColor.CYAN_BOLD}")
+    val neededClosersList = mutableListOf<Char>()
+    while (openersStack.isNotEmpty()) {
+        neededClosersList.add(chunkChars[openersStack.removeLast()]!!)
+    }
+    return neededClosersList;
 }
 
 private fun partTwo(pt: Int = 2) {
     val input = InputUtil.readFileAsStringList("day10/input.txt")
-    val addedChars = mutableListOf<List<Char>>()
 
-    //filter to only lines that need to be completed
-    val incompleteLines = input.filter { line ->
-        var returnVal = false;
-        try {
-            var i = 0;
-            while (i < line.length) {
-                i = getClosingChar(line, i).second;
-            }
-        } catch (e: Exception) {
-            if (e.message?.startsWith("incomplete line,") == true) {
-                returnVal = true
-            };
-        }
-        returnVal;
-    }
-
-    for (l in incompleteLines) {
-        val addedList = mutableListOf<Char>()
-        var line = l
-        while (true) {
-            try {
-                var i = 0;
-                while (i < line.length) {
-                    i = getClosingChar(line, i).second + 1;
-                }
-                break;
-            } catch (e: Exception) {
-                if (e.message?.startsWith("incomplete line,") == true) {
-                    val nextChar = e.message!!.last()
-                    line += nextChar;
-                    addedList.add(nextChar);
-                }
-            }
-        }
-        addedChars.add(addedList);
-    }
-
-    val scores = addedChars.map { listOfAddedChars ->
+    val scores = input.map(::getNeededCharsToComplete).filterNot { it.isEmpty() }.map { listOfAddedChars ->
         //Start with a total score of 0. Then, for each character,
         var score = 0L;
         listOfAddedChars.forEach {
