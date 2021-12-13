762427755b20d9c38d1cad84e3aebac59e0640ec
Dallas Sanchez <dman012@gmail.com>
2021-12-13 17:14:24 -0500

    fix some formatting and make sure col-row syntax is consistent

--- 2021/src/main/kotlin/Day13.kt
+++ 2021/src/main/kotlin/Day13.kt
@@ -1,41 +1,45 @@
 private fun main() {
-    //pt 1 -
-    //pt 2 -
+    //pt 1 - 678
+    //pt 2 - ECFHLHZF
     TimeUtil.startClock(1, ::partOne)
     TimeUtil.startClock(2, ::partTwo)
 }
 
 private fun foldMap(map: MutableMap<Pair<Int, Int>, Char>, instruction: Pair<Char, Int>) {
-    //map<y, x>   (col, row)
+    //map<x, y> to Char  (col, row)
+    //map<col, row> -
     val fold = instruction.second;
-    if (instruction.first == 'x') {
-
-        val keysToAdd = map.keys.filter { it.second > fold }.map{ keyBeingFolded ->
-            keyBeingFolded.copy(second = fold - (keyBeingFolded.second - fold))
+    when (instruction.first) {
+        'x' -> { //flip col
+            val keysToAdd = map.keys.filter { it.first > fold }.map { keyBeingFolded ->
+                keyBeingFolded.copy(first = 2 * fold - keyBeingFolded.first)
+            }
+            val keysToRemove = map.keys.filter { it.first > fold }
+            keysToAdd.forEach { map.putIfAbsent(it, '#') }
+            keysToRemove.forEach { map.remove(it) }
         }
-        val keysToRemove = map.keys.filter { it.second > fold }
-        keysToAdd.forEach { map.putIfAbsent(it, '#') }
-        keysToRemove.forEach { map.remove(it) }
-    } else {
-        val keysToAdd = map.keys.filter { it.first > fold }.map{ keyBeingFolded ->
-            keyBeingFolded.copy(first = fold - (keyBeingFolded.first - fold))
+        'y' -> { //flip row
+            val keysToAdd = map.keys.filter { it.second > fold }.map { keyBeingFolded ->
+                keyBeingFolded.copy(second = 2 * fold - keyBeingFolded.second)
+            }
+            val keysToRemove = map.keys.filter { it.second > fold }
+            keysToAdd.forEach { map.putIfAbsent(it, '#') }
+            keysToRemove.forEach { map.remove(it) }
         }
-        val keysToRemove = map.keys.filter { it.first > fold }
-        keysToAdd.forEach { map.putIfAbsent(it, '#') }
-        keysToRemove.forEach { map.remove(it) }
     }
 }
 
 private fun partOne(pt: Int = 1) {
     val input = InputUtil.readFileAsStringList("day13/input.txt", "\n\n")
     val instructions: List<Pair<Char, Int>> = input[1].split("\n").map {
-        val direction = if (it.contains("x")) { 'x' } else { 'y' }
+        val direction = if (it.contains("x")) 'x' else 'y'
         direction to it.substringAfter("=").toInt()
     }
+
     val map: MutableMap<Pair<Int, Int>, Char> = input[0].split("\n").map {
         val col = it.substringBefore(",").toInt();
         val row = it.substringAfter(",").toInt();
-        Pair(row, col) to '#'
+        Pair(col, row) to '#'
     }.toMap().toMutableMap()
 
     foldMap(map, instructions[0])
@@ -47,28 +51,31 @@ private fun partOne(pt: Int = 1) {
 private fun partTwo(pt: Int = 2) {
     val input = InputUtil.readFileAsStringList("day13/input.txt", "\n\n")
     val instructions: List<Pair<Char, Int>> = input[1].split("\n").map {
-        val direction = if (it.contains("x")) { 'x' } else { 'y' }
+        val direction = if (it.contains("x")) 'x' else 'y'
         direction to it.substringAfter("=").toInt()
     }
     val map: MutableMap<Pair<Int, Int>, Char> = input[0].split("\n").map {
         val col = it.substringBefore(",").toInt();
         val row = it.substringAfter(",").toInt();
-        Pair(row, col) to '#'
+        Pair(col, row) to '#'
     }.toMap().toMutableMap()
 
     instructions.forEach { instruction -> foldMap(map, instruction) }
 
+    printMap(map)
+//    val answer = map.values.count();
+//    println("pt $pt answer: ${answer colorize ConsoleColor.CYAN_BOLD}")
+}
+
+private fun printMap(map: MutableMap<Pair<Int, Int>, Char>) {
     val maxFirst = map.keys.map { it.first }.maxOrNull()!!
     val maxSecond = map.keys.map { it.second }.maxOrNull()!!
-
-    for (i in 0..maxFirst) {
-        for (j in 0..maxSecond) {
-            print(map.getOrDefault(i to j, " ") colorize ConsoleColor.BLUE)
+    for (j in 0..maxSecond) {
+        for (i in 0..maxFirst) {
+            print(map.getOrDefault(i to j, ".") colorize ConsoleColor.BLUE)
         }
         println();
     }
-//    val answer = map.values.count();
-//    println("pt $pt answer: ${answer colorize ConsoleColor.CYAN_BOLD}")
 }
 
 
