dee67a5c29e839bc649935937527c751f9514428
Dallas Sanchez <dman012@gmail.com>
2021-12-21 23:59:14 -0500

    day 21 pt 2 done! memoization and recursion for the win

--- 2021/src/main/kotlin/Day21.kt
+++ 2021/src/main/kotlin/Day21.kt
@@ -1,6 +1,6 @@
 private fun main() {
     //pt 1 - 707784
-    //pt 2 -
+    //pt 2 - 157595953724471
     TimeUtil.startClock(1, ::partOne)
     TimeUtil.startClock(2, ::partTwo)
 }
@@ -22,14 +22,23 @@ private class DeterministicDice() {
 
     }
 }
+// list of possible sums from rolling a dirac dice 3 times (each roll producing 1, 2, or 3)
+val diracDicePossibilities = (1..3).flatMap { i -> (1..3)
+        .flatMap { j -> (1..3)
+            .map { k -> i + j + k } } }
 
-private class DiracDicePlayer(var position: Int, val goal: Int = 1000) {
-    var points = 0;
-    fun moveSpaces(num: Int) {
-        var newSpace = (position + num) % 10
-        if (newSpace == 0) newSpace = 10
-        points += newSpace
-        position = newSpace
+private data class DiracDicePlayer(var position: Int, val goal: Int = 1000, var points: Int = 0) {
+    companion object {
+        fun getNewPosition(currPos: Int, numToMove: Int): Int {
+            val newSpace = (currPos + numToMove) % 10
+            return if (newSpace == 0) 10 else newSpace
+        }
+    }
+
+    fun moveSpaces(num: Int, addPoints: Boolean = true): Int {
+        position = getNewPosition(position, num)
+        points += position
+        return position
     }
 
     fun hasWon() = points >= goal
@@ -54,21 +63,46 @@ private fun partOne(pt: Int = 1) {
     println("pt $pt answer $loserPoints * ${dice.rollCounter}: ${answer colorize ConsoleColor.CYAN_BOLD}")
 }
 
+private data class GameResult(var player1Wins: Long = 0, var player2Wins: Long = 0) {
+    fun addRes(result: GameResult) {
+        player1Wins += result.player1Wins
+        player2Wins += result.player2Wins
+    }
+}
+
+val nextTurnMap = mapOf(1 to 2, 2 to 1)
+
+private val resultsCache = mutableMapOf<String, GameResult>()
+private fun playGame(p1Score: Long, p2Score: Long, p1Position: Int, p2Position: Int, turn: Int): GameResult {
+    val result = resultsCache.computeIfAbsent("$p1Score,$p2Score,$p1Position,$p2Position,$turn") {
+        val isP1Turn = turn == 1;
+        if (p1Score >= 21) GameResult(1, 0)
+        else if (p2Score >= 21) GameResult(0, 1)
+        else {
+            val res = GameResult();
+            for (i in diracDicePossibilities) {
+                if (isP1Turn) {
+                    val newPos = DiracDicePlayer.getNewPosition(p1Position, i);
+                    res.addRes(playGame(p1Score + newPos, p2Score, newPos, p2Position, nextTurnMap[turn]!!))
+                } else {
+                    val newPos = DiracDicePlayer.getNewPosition(p2Position, i);
+                    res.addRes(playGame(p1Score, p2Score + newPos, p1Position, newPos, nextTurnMap[turn]!!))
+                }
+            }
+            res;
+        }
+    }
+    return result;
+}
+
 private fun partTwo(pt: Int = 2) {
     val input = InputUtil.readFileAsStringList("day21/input.txt")
     var player1 = DiracDicePlayer(input[0].substringAfter(": ").toInt(), 21)
     var player2 = DiracDicePlayer(input[1].substringAfter(": ").toInt(), 21)
-    val players = listOf(player1, player2);
-    val dice = DeterministicDice();
 
-    var turnPlayer = player1;
+    val finalResult = playGame(0, 0, player1.position, player2.position, 1)
 
-    while (players.none { it.hasWon() }) {
-        turnPlayer.moveSpaces(dice.roll3Times())
-        turnPlayer = if (turnPlayer == player1) player2 else player1
-    }
-
-    val loserPoints = players.filterNot { it.hasWon() }.map { it.points }[0]
-    val answer = dice.rollCounter * players.filterNot { it.hasWon() }.map { it.points }[0]
-    println("pt $pt answer $loserPoints * ${dice.rollCounter}: ${answer colorize ConsoleColor.CYAN_BOLD}")
+    //ex: 444356092776315 vs 341960390180808
+    val answer = maxOf(finalResult.player1Wins, finalResult.player2Wins)
+    println("pt $pt answer : ${answer colorize ConsoleColor.CYAN_BOLD}")
 }
